a5b434166930c52fb9d26d6936957f0e616c763c
shaopei <sc2457@cornell.edu>
2017-09-24 12:41:56 -0400

    debug

--- After_AlleleDB_pipeline/AfterAlleleDB_strandSpecific.sh
+++ After_AlleleDB_pipeline/AfterAlleleDB_strandSpecific.sh
@@ -1,3 +1,4 @@
+echo 'bash AfterAlleleDB_strandSpecific.sh Min_count'
 # partameters
 Min_count=$1
 echo "Min_count         =$1" 
@@ -25,11 +26,11 @@ R --vanilla --slave --args $(pwd) ${minus_input_file} ${Min_count} ${Max_Pvalus}
 new_name=GM12878_GroSeq_d${d}_dRegion${dRegion}_withStrandSpecific_MinCount${Min_count}_MaxPvalue${Max_Pvalus}.txt
 python ${PL}/TSS_d_regions_CandD_directionality_index_20170612.py ${plus_input_file:0:-4}_MinCount${Min_count}_MaxPvalue${Max_Pvalus}.txt \
 ${minus_input_file:0:-4}_MinCount${Min_count}_MaxPvalue${Max_Pvalus}.txt ${tss_plus_f} ${tss_minus_f} \
-new_name d dRegion
+${new_name} ${d} ${dRegion}
 #python /workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/After_AlleleDB_pipeline/TSS_d_regions_CandD_directionality_index_20170612.py interestingHets_plusNoW.txt interestingHets_minusNoW.txt /workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/After_AlleleDB_pipeline/tss_paired_gm12878_plus.bed /workdir/sc2457/GM_GroSeq_AlleleDB_20170606/test_in_total/After_AlleleDB_pipeline/tss_paired_gm12878_minus.bed test.txt
 
 # Con plus and minus change in the same direction, Did change in opposite direction.
 # filter the Concordant region to remove the Regions with large difference between plus and minus strand (although same direction, but different in amplitude).
-R --vanilla --slave --args $(pwd) new_name < ${PL}/Con_Dis_scatterplot.R
+R --vanilla --slave --args $(pwd) ${new_name} < ${PL}/Con_Dis_scatterplot.R
 # oupput is GM12878_GroSeq_d150_dRegion250_withStrandSpecific_MinCount${Min_count}_MaxPvalue${Max_Pvalus}_ConFiltered.bed
 #GM12878_GroSeq_d150_dRegion250_withStrandSpecific_MinCount5_MaxPvalue1_ConFiltered.bed
